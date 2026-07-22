5ec45fc17cfda68a8a1976920f1c21facba46f4d
MostafaMortada <mortadamostafa313@gmail.com>
2026-07-22 15:36:10 +0300

    Added shit support for more filetypes, will be fixed later

--- src/fileselect.c
+++ src/fileselect.c
@@ -54,6 +54,30 @@ char *fileselectmenu(uint8_t *outfiletype) {
 			case 3:
 				filetype = OS_TYPE_STR;
 				break;
+			case 4:
+				filetype = OS_TYPE_EQU;
+				break;
+			case 5:
+				filetype = OS_TYPE_REAL;
+				break;
+			case 6:
+				filetype = OS_TYPE_REAL_LIST;
+				break;
+			case 7:
+				filetype = OS_TYPE_CPLX;
+				break;
+			case 8:
+				filetype = OS_TYPE_CPLX_LIST;
+				break;
+			case 9:
+				filetype = OS_TYPE_MATRIX;
+				break;
+			case 10:
+				filetype = OS_TYPE_PIC;
+				break;
+			case 11:
+				filetype = OS_TYPE_GDB;
+				break;
 			default:
 				filetype = OS_TYPE_PRGM;
 				break;
@@ -89,42 +113,102 @@ char *fileselectmenu(uint8_t *outfiletype) {
 			gfx_PrintStringXY("Open", 0, 232);
 			gfx_PrintStringXY("<<<", 75, 232);
 			gfx_PrintStringXY(">>>", 145, 232);
-			gfx_SetTextXY(10, 10);
+			//gfx_SetTextXY(10, 10);
 			//gfx_PrintUInt(tab, 2);
 
-			gfx_SetTextXY(0, 10);
-			gfx_SetTextFGColor(tab == 0 ? COLORS_BG : COLORS_FG);
-			gfx_SetTextBGColor(tab == 0 ? COLORS_FG : COLORS_BG);
-			gfx_PrintString(" PRGM ");
-
-			gfx_SetTextFGColor(tab == 1 ? COLORS_BG : COLORS_FG);
-			gfx_SetTextBGColor(tab == 1 ? COLORS_FG : COLORS_BG);
-			gfx_PrintString(" PROT.PRGM ");
-
-			gfx_SetTextFGColor(tab == 2 ? COLORS_BG : COLORS_FG);
-			gfx_SetTextBGColor(tab == 2 ? COLORS_FG : COLORS_BG);
-			gfx_PrintString(" APPVAR ");
-
-			gfx_SetTextFGColor(tab == 3 ? COLORS_BG : COLORS_FG);
-			gfx_SetTextBGColor(tab == 3 ? COLORS_FG : COLORS_BG);
-			gfx_PrintString(" STR ");
+			for (int i = 0; i <= 11; i++) {
+				/*if (i == 0) {
+					gfx_SetTextXY(0, 10);
+				} else if (i == 5) {
+					gfx_SetTextXY(0, 20);
+				}*/
+				gfx_SetTextFGColor(tab == i ? COLORS_BG : COLORS_FG);
+				gfx_SetTextBGColor(tab == i ? COLORS_FG : COLORS_BG);
+				switch (i) {
+					case 0:
+						gfx_SetTextXY(0, 10);
+						gfx_PrintString(" PRGM ");
+						break;
+					case 1:
+						gfx_PrintString(" PROT.PRGM ");
+						break;
+					case 2:
+						gfx_PrintString(" APPVAR ");
+						break;
+					case 3:
+						gfx_PrintString(" STR ");
+						break;
+					case 4:
+						gfx_PrintString(" EQU ");
+						gfx_PrintChar(14);
+						break;
+					case 5:
+						gfx_SetTextXY(0, 20);
+						gfx_PrintString(" REAL ");
+						break;
+					case 6:
+						gfx_PrintString(" REAL LIST ");
+						break;
+					case 7:
+						gfx_PrintString(" CPLX ");
+						break;
+					case 8:
+						gfx_PrintString(" CPLX LIST ");
+						gfx_PrintChar(14);
+						break;
+					case 9:
+						gfx_SetTextXY(0, 30);
+						gfx_PrintString(" MATRIX ");
+						break;
+					case 10:
+						gfx_PrintString(" PICTURE ");
+						break;
+					case 11:
+						gfx_PrintString(" GDB ");
+						break;
+				}
+			}
 
 			gfx_SetTextFGColor(COLORS_FG);
 			gfx_SetTextBGColor(COLORS_BG);
 
-			for (int i = 0; i < 20; i++) {
+			for (int i = 0; i < 18; i++) {
 				if (i + scroll < namelistsize) {
 					if (i + scroll == cursor) {
-						gfx_PrintStringXY(">", 32, i * 10 + 24);
+						gfx_PrintStringXY(">", 32, i * 10 + 44);
 					}
 					uint8_t han = ti_OpenVar(namelist[i + scroll], "r", filetype);
 					if (ti_IsArchived(han) != 0) {
-						gfx_PrintStringXY("*", 40, i * 10 + 24);
+						gfx_PrintStringXY("*", 40, i * 10 + 44);
 					}
-					gfx_SetTextXY(128, i * 10 + 24);
+					gfx_SetTextXY(128, i * 10 + 44);
 					gfx_PrintUInt(ti_GetSize(han), 5);
 					ti_Close(han);
-					gfx_PrintStringXY(namelist[i + scroll], 48, i * 10 + 24);
+					char *name = namelist[i + scroll];
+					gfx_SetTextXY(48, i*10 + 44);
+					switch (name[0]) {
+						case 0x5D: // List
+							if (name[1] < 6) {
+								gfx_PrintChar('L');
+								gfx_PrintChar(name[1] + '1');
+							} else {
+								gfx_PrintString(name + 1);
+							}
+							break;
+						case 0x5C:
+							gfx_PrintChar('[');
+							gfx_PrintChar(name[1] + 'A');
+							gfx_PrintChar(']');
+							break;
+						//case 0xAA:
+						//	gfx_PrintString("Str");
+						//	gfx_PrintUInt(name[1] + 1, 1);
+						//	break;
+						default:
+							gfx_PrintString(name);
+							break;
+					}
+					//gfx_PrintStringXY(namelist[i + scroll], 48, i * 10 + 34);
 				}
 			}
 
@@ -139,9 +223,9 @@ char *fileselectmenu(uint8_t *outfiletype) {
 			if kb_IsDown(kb_KeyDown) {cursor++;}
 			if kb_IsDown(kb_KeyUp) {cursor--;}
 			if (cursor < scroll) {scroll--;}
-			if (cursor > 1000) {cursor = namelistsize - 1; scroll = namelistsize >= 20 ? cursor - 20 : scroll;}
+			if (cursor > 1000) {cursor = namelistsize - 1; scroll = namelistsize >= 18 ? cursor - 18 : scroll;}
 			if (namelist[cursor][0] == '\0') {cursor = 0; scroll = 0;}
-			if (cursor > scroll + 19) {scroll++;}
+			if (cursor > scroll + 17) {scroll++;}
 
 			//while (!kb_AnyKey());
 		}
